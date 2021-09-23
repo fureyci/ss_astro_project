f536420e73f32c3fbfa730d302fd0e6a11556b68
Ciaran Furey <83065792+fureyci@users.noreply.github.com>
2021-09-23 11:37:23 +0100

    Possible skeleton for ensemble model
    Still need to think about the necessary parameters.

--- ensemble.py
+++ ensemble.py
@@ -3,7 +3,60 @@
 Created on Mon Sep 20 16:49:57 2021
 
 @author: Ciaran
+Ensemble space weather forecast.
 """
 
 class Ensemble:
-    pass
+    """ Ensemble model for solar flare forecasting."""
+
+    def __init__(self):
+        pass
+
+    def models_df(self):
+        """ Generate pandas dataframe whose columns represent the
+        forecast of the different models for the specific flare class,
+        and rows represent the date of the forecast.
+
+
+        Returns
+        -------
+        None.
+
+        """
+        return None
+
+    def average(self):
+        """ Perform simple ensemble average on models_df.
+
+
+
+        Returns
+        -------
+        None.
+
+        """
+        pass
+
+    def con_lc(self):
+        """ Perform metric-optimized constrained linear combination of
+        weights.
+
+
+        Returns
+        -------
+        None.
+
+        """
+        pass
+
+    def uncon_lc(self):
+        """ Perform metric-optimized unconstrained linear combination
+        of weights.
+
+
+        Returns
+        -------
+        None.
+
+        """
+        pass
