c535aa00606ebea5d9f507d007b26189416b2960
Ciaran Furey <83065792+fureyci@users.noreply.github.com>
2021-10-08 17:15:44 +0100

    major update to ensemble.py
    Mapped negative values to 0 in __init__() as opposed to every function where ensemble is created, included the constrained linear combination process, changed some comments around. Still need to debug unconstrained linear combination.

--- ensemble.py
+++ ensemble.py
@@ -5,29 +5,22 @@ Created on Mon Sep 20 16:49:57 2021
 @author: fureyci
 Ensemble space weather forecast.
 
-Still need to include methods for un/constrained linear
-combinations. Might also change how data is input into
-the class (sometime in the future).
+To Do:
+Fix unconstrained LC.
+Look at changing how data is loaded.
+Include titles for plots.
 
-Example test of the class so far:
-
-from opening_data import observations, model_names_only, model_csv_list
-from ensemble import Ensemble
-
-# instantiate
-test = Ensemble(model_csv_list, model_names_only, observations)
-
-# visualise performance
-test.visualise_performance(which='reliability')
+Example test included in ens_test.py.
 """
 
 import pandas as pd
 import numpy as np
+from scipy.optimize import minimize
 from metric_utils import plot_reliability_curve, plot_roc_curve
 
 class Ensemble:
     """ Ensemble model for solar flare forecasting.
-    
+
     Will be different depending on user needs. That is, depending on
     the desired flare event to be forecasted (desired_forecasts), the
     desired weighting scheme to be used (desired_weighting), and the
@@ -81,16 +74,18 @@ class Ensemble:
     desired_metric : `str`. The default is None.
         Metric to be optimised. One of:
 
-        | *some metric* : describe
-        | *some metric* : describe
-        | ...
-        
+        | "brier" : Brier Score
+        | "LCC" : Linear Correlation Coefficient.
+        | "MAE" : Mean Absolute Error
+        | *include more*
+
     """
 
     def __init__(self, forecasts, model_names, observations,
                  desired_forecast="M1+", desired_weighting="average",
                  desired_metric=None):
         """Initialise instance of ensemble forecast."""
+
         # list of each model predictions
         self.forecasts = forecasts
 
@@ -109,6 +104,9 @@ class Ensemble:
         # flare
         self.df_of_models, self._obs_vals = self._models_df()
 
+        # map any negative values to 0
+        self.df_of_models[self.df_of_models < 0] = 0
+
         # calculate ensemble prediction according to desired weighting
         # scheme.
         if self.desired_weighting == "average":
@@ -118,14 +116,17 @@ class Ensemble:
             self.forecast = self._performance_history()
 
         elif self.desired_weighting == "constrained":
-            self.forecast = self._con_lc()
+            self.forecast, self.weights = self._linear_combo(constrained=True)
 
-        elif self.desired_weighting == "unconstrained":
-            self.forecast = self._uncon_lc()
+
+       # elif self.desired_weighting == "unconstrained":
+            # *NEED TO DEBUG ULC*
+            # pass
 
         else:
             raise ValueError("desired_weighting must be one of: 'average,'"
-                             " 'history,' 'constrained,' or 'unconstrained.'")
+                             " 'history,' 'constrained'. 'unconstrained' "
+                             " still needs debugging.")
 
     def _models_df(self):
         """ Generate pandas dataframe whose columns represent the
@@ -149,13 +150,15 @@ class Ensemble:
         dates = self.forecasts[0].loc[:,'VALID_DATE'].values
 
         # Dictionary that will store the forecasts for each model.
-        # A dictionary can be passed into pd.DataFrame, where the keys represent
-        # the column headers, and the values represent the column values.
+        # A dictionary can be passed into pd.DataFrame, where the keys
+        # represent the column headers, and the values represent the
+        # column values.
         df_dict = {}
 
         df_dict['Date'] = dates # store the dates in the dataframe
 
-            # now need to load and forecast, depending on flare_class parameter.
+        # now need to load forecast and observations, depending on
+        # flare_class parameter.
         for name, forecast in zip(self.model_names, self.forecasts):
 
             if self.desired_forecast == "C-only":
@@ -199,14 +202,11 @@ class Ensemble:
         """
         headers = list(self.df_of_models) # the column headers of the df
 
-        # map any negative values to 0
-        self.df_of_models[self.df_of_models < 0] = 0
-
         # calculate average (weights = 1 / len(headers))
         ensemble_av = self.df_of_models.sum(axis=1) / len(headers)
 
         return ensemble_av
-    
+
     def _performance_history(self):
         """ Calculate weights based of performance history of the
         models.
@@ -223,10 +223,8 @@ class Ensemble:
         and Space Climate, 10, p.38, Section 3.1:
             - for loop calculates Equation (4)
             - "denominator" is denominator of Equation (3)
-            
+
         """
-        # map negative values to 0
-        self.df_of_models[self.df_of_models < 0] = 0
 
         # there is eventually going to be a weight for each model, so
         # instantiate array of 0s same length as number of models
@@ -257,32 +255,209 @@ class Ensemble:
         ensemble_prediction = np.sum(ensemble_to_be_summed, axis=1)
 
         return ensemble_prediction
-    
-    def con_lc(self):
-        """ Perform metric-optimized constrained linear combination of
-        weights.
+
+    def _linear_combo(self, constrained=True):
+        """Perform linear combination to determine weights.
+
+        Value of "constrained" determines weither the combination is
+        constrained ("constrained" = True) or unconstrained
+        ("constrained" = False).
+
+        Parameters
+        ----------
+        constrained : `bool`. The default is True.
+            Whether the linear combination is constrained (True) or
+            unconstrained (False)
 
         Returns
         -------
-        None.
+        ensemble_prediction : `float`
+            Ensemble prediction.
+
+        Reference
+        ---------
+        Guerra, J.A. et al., 2020. Ensemble forecasting of major solar
+        flares: methods for combining models. Journal of Space Weather
+        and Space Climate, 10, p.38, Sections 3.2, 3.3, 4.1.
+            - initial weights take value [0,1] for constrained case.
+            - initial weights take value [-1,1] for unconstrained case.
+            - Solver executed 500 times, results in distribution that
+                is normal in shape, mean value used as final optimised
+                weight, with std dev used as 10% error
 
         """
-        # NEED TO UPDATE
-        pass
 
-    def uncon_lc(self):
-        """ Perform metric-optimized unconstrained linear combination
-        of weights.
+        def _metric_funct(en_forecast_to_optimise):
+            """Define the metrics to be used to create the ensemble.
+            Depending on self.desired_metric, the options are:
 
-        Returns
-        -------
-        None.
+            | "brier" : Brier Score
+            | "LCC" : Linear Correlation Coefficient.
+            | "MAE" : Mean Absolute Error
+            | *include more*
 
-        """
-        # NEED TO UPDATE
-        pass
-    
-    def visualise_performance(self, which="both", title=None, savename=None):
+
+            Parameters
+            ----------
+            en_forecast_to_optimise : `np.array`
+                Forecast of ensemble whose weights have NOT been
+                optimised, same shape as self._obs_vals.
+                Provided by _optimise_funct().
+
+            Returns
+            -------
+            funct : `np.array`
+                Metric function.
+
+            """
+
+            if self.desired_metric == 'brier':
+                funct = np.mean(
+                    (en_forecast_to_optimise - self._obs_vals) ** 2.0
+                                )
+
+            if self.desired_metric == 'LCC':
+                funct = np.corrcoef(
+                    en_forecast_to_optimise,
+                    self._obs_vals)[0, 1]
+                # [0,1] since corrcoef returns 2d covariance matrix -
+                # diagonal values = 1, and, if covariance matrix is C,
+                # C[i,j] = C[j,i].
+
+            if self.desired_metric == 'MAE':
+                funct = np.mean(
+                    np.abs(en_forecast_to_optimise - self._obs_vals)
+                    )
+
+            # *ADD MORE METRICS*
+
+            return funct
+
+        def _optimise_funct(ws_ini):
+            """Function to be optimised. Calculates linear combination
+            using the initial guesses for the weights, passes this into
+            _metric_funct(), obtains the desired metric function and
+            returns it. This will be used in the
+            scipy.optimize.minimize() routine (implemented later)
+
+            Parameters
+            ----------
+            ws_ini : `np.array`
+                Array of initial guess for weights.
+
+            Returns
+            -------
+            ofunct : `np.array`
+                Function to be optimised.
+
+            """
+
+            # linear combination.
+            # if desired_weighting == "unconstrained", ws_ini includes
+            # the weight for climatology (done later in the function),
+            # and self.df_of_models.values includes the column for
+            # climatology, which was done in __init__.
+            # print(self.df_of_models)
+            combination = np.sum(
+                    ws_ini * self.df_of_models.values,
+                    axis=1
+                    )
+
+            # function to optimise
+            ofunct = _metric_funct(combination)
+
+            if self.desired_metric == 'LCC':
+                ofunct = -1 * ofunct
+
+            return ofunct
+
+        # define number of models (used multiple times next)
+        number_of_models = len(self.model_names)
+
+        # number of times to repeat optimisation process, same as
+        # Geurra et al. 2020.
+        REPITITIONS = 50
+
+        if constrained is True:
+            # # initial weights, random values between 0 and 1
+            # initial_weights = np.random.rand(number_of_models)
+
+            # to be used as "jac" in constraints
+            weights = np.ones(number_of_models)
+
+            # define bounds
+            bounds = np.full((number_of_models,2), (0,1))
+
+            # set up array of zeros that will store the optimised
+            # weights from each repitition.
+            accumulated_weights = np.zeros(
+                (REPITITIONS, number_of_models)
+                )
+
+        else:
+            # *NEED TO DEBUG ULC*
+            pass
+
+        # Now define the constraints:
+        # "type": constraint type, "eq" for equality, meaning that "fun"
+        #   must result to zero.
+        # "fun" : function defining the constraint. That is, all of the
+        #   weights have to sum up to 1, since "type"="eq".
+        # "jac" : jacobion of "fun"
+
+        consts = ({"type": "eq",
+                    "fun": lambda ws: np.sum(ws) - 1, # weights add to 0
+                    "jac": lambda ws: weights})
+
+        # repeat optimisation procedure to account for sensitivity of
+        # the results of the optimised weights to the choice of the
+        # initial guess.
+        for i in range(REPITITIONS):
+            if constrained is True:
+                # initial weights, random values between 0 and 1
+                initial_weights = np.random.rand(number_of_models)
+
+            else:
+                # *NEED TO DEBUG ULC*
+                pass
+
+            result = minimize(_optimise_funct,
+                              initial_weights,
+                              constraints=consts,
+                              bounds=bounds,
+                              method="SLSQP",
+                              jac=False,
+                              options={
+                                  'disp': False,
+                                  'maxiter': 10000,
+                                  'eps': 0.001
+                                  }
+                              )
+
+            # store this set of weights, and repeat.
+            accumulated_weights[i] = result.x
+
+        if constrained is True:
+            optimised_weights = np.sum(
+                accumulated_weights, axis=0
+                ) / float(REPITITIONS) # average weights
+
+            # calculate the ensemble prediction with the optimised
+            # weights.
+            ensemble_prediction = np.sum(
+                optimised_weights * self.df_of_models.values,
+                axis=1
+                )
+        else:
+            # *NEED TO DEBUG ULC*
+            pass
+
+        # map any negative values to 0
+        ensemble_prediction[ensemble_prediction < 0] = 0
+
+        return ensemble_prediction, optimised_weights
+
+    def visualise_performance(self, which="both"):
         """
         Visualise how the ensemble performed through either a
         reiliability diagram, ROC plot, or both, depending on the value
@@ -309,12 +484,14 @@ class Ensemble:
                                               self.forecast,
                                               n_bins=20)
             return ax1, ax2
-        
+
         elif which == "ROC":
-            ax = plot_roc_curve(self._obs_vals, self.forecast)
-            return ax
-        
+            roc_ax = plot_roc_curve(self._obs_vals, self.forecast)
+            return roc_ax
+
         elif which == "both":
-            # NEED TO UPDATE
-            pass
-        
+            ax1, ax2 = plot_reliability_curve(self._obs_vals,
+                                              self.forecast,
+                                              n_bins=20)
+            ax3 = plot_roc_curve(self._obs_vals, self.forecast)
+            return ax1, ax2, ax3
